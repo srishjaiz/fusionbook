90a586c89932785d3bb9865c97773c0c30c32298
Srishti <srishjaiz@gmail.com>
2019-09-03 15:32:01 +0530

    variable stars

--- components/html/rating.js
+++ components/html/rating.js
@@ -1,106 +1,115 @@
-export default function f(el,svg_width,svg_height,star_stroke,star_fill,star_strokewidth,direction){
-    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttribute("width",svg_width);
-    svg.setAttribute("height",svg_height);
+export default function f(el,svg_width,svg_height,star_stroke,star_fill,star_strokewidth,direction,N=5){
     
-    var start,start2,start3,start4,start5;
-
     //checking general cases
     svg_height=Number(svg_height);
-    if((typeof svg_height) !== "number"){
-        throw new TypeError("Invalid svg height, only enter a number");
+    if(!svg_height){
+        console.error("Invalid svg height, only enter a number");
+        return;
     }
     else{
         console.log("height ok");
-    }
-    svg_width=Number(svg_width);
-    if((typeof svg_width) !== "number"){
-        throw new TypeError("Invalid svg width, only enter a number");
-    }
-    else{
-        console.log("width ok");
-    }
-    star_stroke=String(star_stroke);
-    if((typeof star_stroke) !== "string"){
-        throw new TypeError("Invalid stroke color, only enter a string");
-    }
-    else{
-        console.log("stroke color ok");
-    }
-    star_fill=String(star_fill);
-    if((typeof star_fill) !== "string"){
-        throw new TypeError("Invalid fill color, only enter a string");
-    }
-    else{
-        console.log("fill color ok");
-    }
-
-    star_strokewidth=Number(star_strokewidth);
-    if((typeof star_strokewidth) !== "number"){
-        throw new TypeError("Invalid stroke width, only enter a number");
-    }
-    else{
-        console.log("stroke color ok");
-    }
-
-
-    var box=0;
-    if(direction=="horizontal"){
-        if(svg_height<=svg_width){
-            if(svg_width>=svg_height*5){
-                box=svg_height;
-            }
-            else{
-                throw new TypeError("Width cannot fit all 5 stars");
-            }   
-        }
-        else{
-            throw new TypeError("For horizontal, height cannot be greater than width");
-        }
-
-
-    start=((box + (svg_width - box*5)+ star_strokewidth) / 2) + " " + ((svg_height - box +star_strokewidth) / 2);
-    start2=((box +2*box + (svg_width - box*5)+ star_strokewidth) / 2) + " " + ((svg_height - box +star_strokewidth) / 2);
-    start3=((box +4*box + (svg_width - box*5)+ star_strokewidth) / 2) + " " + ((svg_height - box +star_strokewidth) / 2);
-    start4=((box +6*box + (svg_width - box*5)+ star_strokewidth) / 2) + " " + ((svg_height - box +star_strokewidth) / 2);
-    start5=((box +8 *box + (svg_width - box*5)+ star_strokewidth) / 2) + " " + ((svg_height - box +star_strokewidth) / 2);
-    }
-    else if(direction=="vertical"){
-        if(svg_width<=svg_height){
-            if(svg_height>=svg_width*5){
-                box=svg_width;
-            }
-            else{
-                throw new TypeError("Height cannot fit all 5 stars");
-            }   
-        }
-        else{
-            throw new TypeError("For vertical, width cannot be greater than height");
-        }
-
-
-    start=((box + (svg_width - box)+ star_strokewidth) / 2) + " " + ((svg_height - box*5 +star_strokewidth) / 2);
-    start2=((box + (svg_width - box)+ star_strokewidth) / 2) + " " + ((2*box +svg_height - box*5 +star_strokewidth) / 2);
-    start3=((box + (svg_width - box)+ star_strokewidth) / 2) + " " + ((4*box +svg_height - box*5 +star_strokewidth) / 2);
-    start4=((box + (svg_width - box)+ star_strokewidth) / 2) + " " + ((6*box +svg_height - box*5 +star_strokewidth) / 2);
-    start5=((box + (svg_width - box)+ star_strokewidth) / 2) + " " + ((8*box +svg_height - box*5 +star_strokewidth) / 2);
-    }
-    else{
-        throw new TypeError("Invalid Direction, give horizontal or vertical only");
-    }
-
-
-
-    createStar(svg,box,star_strokewidth,star_fill,star_stroke,svg_width,svg_height,start);
-    createStar(svg,box,star_strokewidth,star_fill,star_stroke,svg_width,svg_height,start2);
-    createStar(svg,box,star_strokewidth,star_fill,star_stroke,svg_width,svg_height,start3);
-    createStar(svg,box,star_strokewidth,star_fill,star_stroke,svg_width,svg_height,start4);
-    createStar(svg,box,star_strokewidth,star_fill,star_stroke,svg_width,svg_height,start5);
     
-   // var svg_div= document.getElementById("svg-div");
-    el.appendChild(svg);
-}
+        svg_width=Number(svg_width);
+        if(!svg_width){
+            console.error("Invalid svg width, only enter a number");
+            return;
+        }
+        else{
+            console.log("width ok");
+        
+            star_stroke=String(star_stroke);
+            if(!checkHex(star_stroke)){
+                console.error("Only hex value is allowed");
+                return;
+            }
+            else{
+                console.log("stroke color ok");
 
+                star_fill=String(star_fill);
+                if(!checkHex(star_fill)){
+                    console.error("Only hex value is allowed");
+                    return;
+                }
+                else{
+                    console.log("star fill ok");
+                
+
+                    star_strokewidth=Number(star_strokewidth);
+                    if((typeof star_strokewidth) !== "number"){
+                        console.error("Invalid stroke width, only enter a number");
+                        return;
+                    }
+                    else{
+                        if(star_strokewidth>10){
+                            console.error("stroke width cannot be greater than 10");  
+                            return;          
+                        }
+                        else{
+                            console.log("stroke width ok");
+
+                            N=Number(N);
+                            if(!N){
+                                console.error("Invalid no of stars given, only enter a number");
+                                return;
+                            }
+                            else{
+                                console.log("no of stars ok");
+                                var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                                svg.setAttribute("width",svg_width);
+                                svg.setAttribute("height",svg_height);
+                                var box=0;
+                                if(direction=="horizontal"){
+                                    if(svg_height<=svg_width){
+                                        if(svg_width>=svg_height*N){
+                                            box=svg_height;
+                                        }
+                                        else{
+                                            console.error("Width cannot fit all 5 stars");
+                                            return;
+                                        }   
+                                    }
+                                    else{
+                                        console.error("For horizontal, height cannot be greater than width");
+                                        return;
+                                    }
+
+                                    for(let i=0;i<N;i++){
+                                        var start=((box +(2*i)*box + (svg_width - box*N)+ star_strokewidth) / 2) + " " + ((svg_height - box +star_strokewidth) / 2);
+                                        createStar(svg,box,star_strokewidth,star_fill,star_stroke,svg_width,svg_height,start);
+                                    }
+                                }
+                                else if(direction=="vertical"){
+                                    if(svg_width<=svg_height){
+                                        if(svg_height>=svg_width*N){
+                                            box=svg_width;
+                                        }
+                                        else{
+                                            console.error("Height cannot fit all 5 stars");
+                                            return;
+                                        }   
+                                    }
+                                    else{
+                                        console.error("For vertical, width cannot be greater than height");
+                                        return;
+                                    }
+                                    for(let i=0;i<N;i++){
+                                        var start=((box + (svg_width - box)+ star_strokewidth) / 2) + " " + (((2*i)*box +svg_height - box*N +star_strokewidth) / 2);
+                                        createStar(svg,box,star_strokewidth,star_fill,star_stroke,svg_width,svg_height,start);
+                                    }
+                                }
+                                else{
+                                    console.error("Invalid Direction, give horizontal or vertical only");
+                                    return;
+                                }
+                                el.appendChild(svg);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 function createStar(svg,box,star_strokewidth,star_fill,star_stroke,svg_width,svg_height,start){
     var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
     newElement.setAttribute("d",
@@ -118,8 +127,11 @@ function createStar(svg,box,star_strokewidth,star_fill,star_stroke,svg_width,svg
     newElement.style.stroke = star_stroke; 
     newElement.style.strokeWidth = star_strokewidth;
     newElement.style.fill=star_fill;
-    //newElement.style.stroke-linejoin="round";
-    //newElement.style.strokeLinecap="round";
     newElement.style.strokeLinejoin="round";
     svg.appendChild(newElement);
+}
+
+//function to check if input is hex value or not
+function checkHex(value){
+	return /^#([A-Fa-f0-9]{3}$)|([A-Fa-f0-9]{6}$)/.test(value)
 }
